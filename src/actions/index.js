5adb20d7f99820e0a34c2c48e506e640c8a1bc4f
Carolina Borim <carolina.borim@gmail.com>
2019-10-16 23:49:50 +0200

    Refactor to ES2015 syntax.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import jsonPlaceholder from '../api/jsonPlaceholder';
 
-export const fetchPosts = () => 
-    return async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () =>
+    async dispatch => {
+        const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+        dispatch({ type: 'FETCH_POSTS', payload: response })
 
-};
+    };
 
-    export default fetchPosts;
+export default fetchPosts;
